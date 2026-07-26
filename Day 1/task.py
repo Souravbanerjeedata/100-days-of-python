4def7041e212060e34c3fdff87729a21af29dfe1
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-07-26 10:39:28 +0530

    Day 1 complete

--- Day 1/task.py
+++ Day 1/task.py
@@ -2,4 +2,19 @@
 # print('Hello World!')
 
 # Inputting something and saving the variable
-name = input("What's your name?")
+# name = input("What's your name?")
+# print("Hello " + name + "!")
+
+# Varibales
+# get the length of the input variable
+# name = input("What's your name?\n")
+# print("Length: " + str(len(name)))
+
+
+# Final challenge for the day
+# create a band name generator, by merging your city name and your pet name.
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your band name could be '{city_name} {pet_name}' or '{pet_name} {city_name}'.")
