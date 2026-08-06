fe7b211e6a5cc5970fe3189adaf7fbe408bd21a0
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-06 13:28:56 +0530

    day 20 - controling the snake with key press

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -2,11 +2,16 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snakes = []
         self.create_snake()
+        self.head = self.snakes[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -21,4 +26,20 @@ class Snake:
             new_x = self.snakes[snake_index - 1].xcor()
             new_y = self.snakes[snake_index - 1].ycor()
             self.snakes[snake_index].goto(new_x, new_y)
-        self.snakes[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+         self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
