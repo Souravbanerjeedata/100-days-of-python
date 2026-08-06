f8dec7e10a1cfa5c7c9200d140742c0ad9fd3ac0
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-06 18:27:22 +0530

    day 20 - detect snake collision with wll

--- Day 20 - 21/scoreboard.py
+++ Day 20 - 21/scoreboard.py
@@ -12,6 +12,10 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.update_scoreboard()
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
