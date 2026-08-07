f18c0e9db5fd613e0f75fbe310aebd2091e261c9
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-07 12:44:24 +0530

    day 22 - write the ball class and make the ball move

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,16 +1,17 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(800, 600)
+screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
-
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,7 +21,9 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
