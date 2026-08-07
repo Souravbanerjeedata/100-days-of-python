c2d86c4cc89ea26d20a05a004ff923627a46eae2
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-07 20:22:23 +0530

    day 23 - detect when player has crossed road successfully and then increase the car speed

--- Day 23/main.py
+++ Day 23/main.py
@@ -16,7 +16,17 @@ game_on = True
 while game_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
 
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_on = False
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
 screen.exitonclick()
