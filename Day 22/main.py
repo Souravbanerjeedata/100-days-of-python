10dfd4e94dc59261a2f2001aec923a864e623942
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-07 13:46:25 +0530

    day 22 - detecting when the ball goes out of bounds

--- Day 22/main.py
+++ Day 22/main.py
@@ -33,4 +33,12 @@ while game_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # detect when R paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # detect when L paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
