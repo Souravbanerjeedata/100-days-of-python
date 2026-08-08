85b495d3e2352345fab055571c6faf831eaa302f
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-08 18:53:06 +0530

    day 25 - read csv data without and with pandas

---
+++ Day 25/main.py
@@ -0,0 +1,20 @@
+# give me the temp data from csv
+# import csv
+
+# with open("./weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+# print(temperatures)
+
+
+# Do the same but with pandas
+
+import pandas
+
+data = pandas.read_csv("./weather_data.csv")
+
+print(data["temp"])
