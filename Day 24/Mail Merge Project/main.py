7dd1ced02047bf7ebe7a993e76bcd20a59c1901c
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-08 17:08:31 +0530

    day 24 - mail merge project completed

--- Day 24/Mail Merge Project/main.py
+++ Day 24/Mail Merge Project/main.py
@@ -6,24 +6,15 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
 
-with open("./Input/Letters/starting_letter.txt", "r") as letter_file:
-    letter_lines = letter_file.readlines()
-with open("./Input/Names/invited_names.txt", "r") as names_file:
-    names_list = names_file.readlines()
+with open("./Input/Names/invited_names.txt", mode="r") as names_file:
+    names = names_file.readlines()
 
-new_letter_lines = []
-for each_word in letter_lines:
-    new_letter_lines.append(each_word.strip())
-new_names_list = []
-for name in names_list:
-    new_names_list.append(name.strip())
-    
-letter = "\n".join(new_letter_lines)
-
-for name in new_names_list:
-    new_letter = letter.replace(f"Dear [name],", "Dear {name},")
-    with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as letter:
-        letter.write(f"{new_letter}")
-    
-    
+with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter-to-{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
