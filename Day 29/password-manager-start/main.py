a27d4cffdba21e27a6a26d697a54f76ca7de03e2
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-20 08:00:04 +0530

    day 29 - UI setup for password manager

--- Day 29/password-manager-start/main.py
+++ Day 29/password-manager-start/main.py
@@ -14,15 +14,37 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 logo_path = os.path.join(script_dir, "logo.png")
 window = Tk()
 window.title('Password Manager')
-# window.minsize(width=200, height=200)
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # logo
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file=logo_path)
 canvas.create_image(100,100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+# Input span
+# Labels
+website_label = Label(text='Website:')
+website_label.grid(row=1, column=0)
+username_label = Label(text='Email/Username:')
+username_label.grid(row=2, column=0)
+password_label = Label(text='Password:')
+password_label.grid(row=3, column=0)
 
+# Entries
+website_entry = Entry(width=35)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
+username_entry = Entry(width=35)
+username_entry.insert(0, 'sourav@email.com')
+username_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_button = Button(text='Generate Password',width=14)
+generate_button.grid(row=3, column=2)
+add_button = Button(text='Add', width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
