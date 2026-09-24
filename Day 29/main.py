73a3df0f8cee4396185051618cc2b2a5f35ac120
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-09-24 09:33:40 +0530

    day 30 - try catch error

--- Day 29/main.py
+++ Day 29/main.py
@@ -20,7 +20,7 @@ def generate_password():
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
     password_list = password_letters + password_symbols + password_numbers
-
+    
     shuffle(password_list)
     password = "".join(password_list)
     password_entry.insert(0, password)
